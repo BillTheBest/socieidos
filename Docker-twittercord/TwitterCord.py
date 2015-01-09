f45baac59d1f51b96d872a07403f265e0ccc7555
Clinton Kitson <clintonskitson@gmail.com>
2015-01-08 22:32:13 -0800

    first commit

---
+++ Docker-twittercord/TwitterCord.py
@@ -0,0 +1,213 @@
+from flask import Flask, render_template
+from pprint import pprint, pformat
+import logging
+import requests
+from threading import Thread
+import boto
+from boto.s3.connection import OrdinaryCallingFormat
+from boto.s3.key import Key
+from boto.s3.lifecycle import Lifecycle, Expiration
+import uuid
+from TwitterAPI import TwitterAPI,TwitterRestPager
+from datetime import timedelta
+from default_config import *
+import random
+import time
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+import json
+
+scheduler = BackgroundScheduler()
+
+cache_opts = {
+    'cache.type': 'memory',
+}
+cache = CacheManager(**parse_cache_config_options(cache_opts))
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s: '
+                                '%(levelname)s: '
+                                '%(funcName)s(): '
+                                '%(lineno)d:\t'
+                                '%(message)s')
+
+app = Flask(__name__)
+img_session = requests.Session()
+
+print(lc_s3_connect)
+s3conn = eval(lc_s3_connect)
+
+while True:
+    try:
+        bucket = s3conn.get_bucket(lc_s3_bucket_name)
+        break
+    except Exception,e:
+        logging.error(str(e))
+
+def check_coords(grid,point):
+   return grid[0] < point[1] < grid[2] and grid[1] < point[0] < grid[3]
+
+def watch_tweet_stream(s3_store,
+                       s3_store_in,
+                       s3_object_expiration_time,
+                       twitter_keys=[],
+                       twitter_request='statuses/filter',
+                       twitter_filter={'track':''},
+                       check_geo_coords=False,
+                       geo_box_coords_long_lat='',
+                       check_retweeted_status=False,
+                       check_possibly_sensitive=False):
+    api = TwitterAPI(twitter_keys[0],twitter_keys[1],twitter_keys[2],twitter_keys[3])
+
+    while True:
+        try:
+            twitter_filter['count'] = 100         
+            response = TwitterRestPager(api,twitter_request,twitter_filter)
+            print(str(twitter_filter))
+            for item in response.get_iterator(wait=10):
+                if 'text' in item:
+                    if check_retweeted_status and 'retweeted_status' in item and item['retweeted_status']:
+                        logging.info("Tweet is a retweet.")
+                        continue
+                    if check_possibly_sensitive and 'possibly_sensitive' in item and item['possibly_sensitive']:
+                        logging.info("Tweet is possibly sensitive.")
+                        continue
+                    if check_possibly_sensitive and 'retweet' in item and 'possibly_sensitive' in item['retweet'] and item['retweet']['possibly_sensitive']:
+                        logging.info("Tweet is a retweet and possibly sensitive.")
+                        continue            
+                    if check_geo_coords and item['geo'] is None:
+                        logging.info("Geo tag not set.")
+                        continue
+                    if check_geo_coords and not check_coords(get_coord_array(geo_box_coords_long_lat),item['geo']['coordinates']):
+                        logging.info("Tweet not in region.")
+                        continue
+                    if 'entities' in item and 'media' in item['entities']:
+                        logging.info('Tweet: '+'@'+item['user']['screen_name']+':'+item['text'])
+                        for media_object in item['entities']['media']:
+                            if media_object['type'] == 'photo':
+                                    if s3_store:
+                                        logging.info("Dispatching thread to capture for: " + media_object['media_url'] + " with filter: " + str(twitter_filter) + " and storing in " + s3_store_in.lower())
+                                        t = Thread(target=capture_photo_to_object,args=(s3_object_expiration_time,media_object['media_url'],s3_store_in.lower(),))
+                                        t.start()
+                                    else:
+                                        logging.info('Not storing tweet since s3_store is False.')
+                            else:
+                                logging.info('Tweet media is not a photo.')
+                    else:
+                        logging.info('Tweet does not have media.')
+                elif 'message' in item and item['code'] == 88:
+                    print 'SUSPEND, RATE LIMIT EXCEEDED: %s\n' % item['message']
+                break
+        except Exception,e:
+            logging.error(str(e))
+            logging.debug("Sleeping for 30 seconds")
+            time.sleep(30)
+
+
+
+def get_coord_array(geo_box_coords_long_lat):
+    return [float(n) for n in geo_box_coords_long_lat.split(',')]
+
+def delete_old_keys(store_in,object_max_age):
+    list_of_keys = list(bucket.list(store_in))
+    current_time = time.time()
+    to_delete = []
+    for key in list_of_keys:
+        hashtag,timestamp,guid = key.name.split('/')
+        timestamp = int(timestamp)
+        if len(list_of_keys) > lc_min_items and current_time > timestamp + object_max_age:
+            to_delete.append(key)
+            list_of_keys.remove(key)
+
+    results = bucket.delete_keys(to_delete)
+    logging.info("Deleted")
+    logging.info("Errors")
+    logging.info(results.errors)
+
+
+
+def capture_photo_to_object(s3_object_expiration_time,
+                            url=None, 
+                            hashtag=None):
+    """
+
+    :param url: string
+    :return: :rtype: string
+    """
+    if url is None: return False
+
+    expiry_time = eval(s3_object_expiration_time)
+    image_response = img_session.get(url)
+    if image_response.status_code == 200:
+        logging.debug("Grabbed image from %s" % url)
+        guid = str(uuid.uuid4())
+        k = Key(bucket)
+        timestamp = str(int(time.time()))
+        k.key = "/".join([hashtag,timestamp,guid])
+        logging.debug("Uploading %s as %s" % (k.key,image_response.headers.get('content-type')))
+        k.set_contents_from_string(image_response.content)
+        return k.key
+    else:
+        logging.warning("Failed to grab %s" % url)
+        return None
+
+@cache.cache('get_keys_for_hashtag', expire=lc_get_objects_query_cache_seconds)
+def get_keys_for_hashtag(hashtag,return_limit=25,):
+    candidates = list(bucket.list(hashtag))
+    max_return = min(len(candidates),return_limit)
+    return (candidates[:-max_return])[::-1]
+
+def get_object_url_from_key(key,s3_object_url_timeout):
+    return key.generate_url(s3_object_url_timeout)
+
+
+
+watch = Thread(target=watch_tweet_stream, args=(eval(lc_s3_store),
+                                                lc_s3_store_in,
+                                                lc_s3_object_expiration_time,
+                                                eval(lc_twitter_keys),
+                                                lc_twitter_request,
+                                                eval(lc_twitter_filter),
+                                                eval(lc_check_geo_coords),
+                                                lc_geo_box_coords_long_lat,
+                                                eval(lc_check_retweeted_status),
+                                                eval(lc_check_possibly_sensitive),
+                                                ))
+watch.daemon = True
+watch.start()
+
+def create_hash_objects(dict):
+    return {
+        'response': {
+            'requested_objects':[dict['s3_store_in']],
+            'objects':[{ 'object_url':url} for url in dict['urls'] ],
+            'type':"image",
+            'source':"twitter",
+            'metdata':{}
+        }
+    }
+
+def create_array_urls(s3_object_url_timeout,s3_store_in):
+    urls = [get_object_url_from_key(key,s3_object_url_timeout) for key in get_keys_for_hashtag(s3_store_in)]
+    dict = {'s3_store_in':lc_s3_store_in,'urls':urls}
+    return dict
+
+@app.route('/')
+def dashboard():
+    dict = create_array_urls(lc_s3_object_url_timeout,lc_s3_store_in)
+    objects = create_hash_objects(dict)
+    return render_template('default.html',objects=objects['response']['objects'])
+
+@app.route('/v1/objects', methods=['GET'])
+def get_objects():
+    dict = create_array_urls(lc_s3_object_url_timeout,lc_s3_store_in)
+    objects = create_hash_objects(dict)
+    return json.dumps(objects,sort_keys = False, indent = 4) 
+
+if __name__ == '__main__':
+    scheduler.add_job(delete_old_keys,'interval',args=(lc_s3_store_in,lc_s3_object_max_age,),minutes=lc_job_s3_interval_delete_old_objects_minutes)
+    scheduler.start()
+    port = 8080
+    app.run(debug=False,port=int(port),host='0.0.0.0',threaded=True)
+
---
+++ README.md
@@ -0,0 +1,163 @@
+# Socieidos
+A collection of micro-services that actively watches multiple social media mediums for specific media and retains this data for real-time consumption and analytics use cases.
+
+
+
+Summary
+=======
+There are a range of social media platforms that exist today that are very popular to different types of users.  The information and interaction these platforms expose can be very valuabl to anyone who can properly store and consume the information.
+
+**Socieidos** will have micro-services that connect to social media platforms through public APIs perform a DVR or keep a photographic memory with relevant metadata for objects that are available.  It will then expose a common API through distributed micro-services allowing a consumer to use this information.
+
+Parameters
+==========
+The following parameters can be configured by either the ```/config/custom_config.py``` file or environment variables.  The environment variables will take priority.
+
+
+	# REQUIRED
+	s3_host = 'ip_or_fqdn'
+	s3_akia = 'wuser1@sanity.local'
+	s3_secret = 'lDcxQF/ez1yAeIQhz3AqglUyUydqdy5l+fK+4x1v'
+	s3_bucket_name = 'social'
+	s3_store_in = 'selfie'
+
+
+	twitter_keys = "['key1','key2','key3','key4']"
+	twitter_filter = "{'track':'selfie'}"
+
+	# OPTIONAL
+	s3_port = 10101
+	s3_connect = "boto.connect_s3(lc_s3_akia,lc_s3_secret,is_secure=False,host=lc_s3_host,port=lc_s3_port,calling_format=OrdinaryCallingFormat())"
+	s3_store = 'True'
+	s3_object_expiration_time = 'timedelta(days=1)'
+	s3_object_url_timeout = 1200
+	s3_object_max_age = 3600
+	job_s3_interval_delete_old_objects_minutes = 1
+	min_items = 50
+
+	get_objects_query_cache_seconds = 5
+
+	twitter_request = 'statuses/filter'
+	geo_box_coords_long_lat = '-125,24,-14,83'
+	check_geo_coords = 'True'
+	check_retweeted_status = 'True'
+	check_possibly_sensitive = 'True'
+
+
+### Definitions
+
+	### REQUIRED
+	s3_host = The FQDN or IP of the s3 interface
+	s3_akia = The s3 access key
+	s3_secret = The s3 secret key
+	s3_bucket_name = The s3 bucket_name
+	s3_store_in = The directory to store objects in, ie. the s3://bucket_name/s3_store_in
+
+
+	twitter_keys = The Twitter credentials
+	twitter_filter = The filter based on the Python Twitter API implementation
+
+	### OPTIONAL
+	s3_port = The s3 port
+	s3_connect = The s3 connection string based on the Python Twitter API implementation
+	s3_store = Whether to actually store data or not (whatif)
+	s3_object_expiration_time = An expression for the amount of expiration time an object has once it is written
+	s3_object_url_timeout = The timeout that URLs that are generated for s3 objects have
+	s3_object_max_age = The maximum time an object should exist before being manually deleted
+	job_s3_interval_delete_old_objects_minutes = How often the delete job is ran
+	min_items = The minimum number of objects to keep around
+
+	get_objects_query_cache_seconds = The amount of seconds to wait before re-querying for the current list of objects
+
+	twitter_request = The Twitter API specific filter
+	geo_box_coords_long_lat = Geographic coordinates listed as SW_longitude,SW_latitude,NE_longitude,NE_latitide
+	check_geo_coords = Whether to decide on processing by checking the coorindates
+	check_retweeted_status = Whether to decide on processing by checking if retweet
+	check_possibly_sensitive = Whether to decide on processing by checking if sensitive material flag is True
+
+Configuration
+=============
+There are two ways to configure the ```TwitterCord``` container.
+
+### Environment Variables
+By running the docker container with ```-e``` parameters, you can specify configuration interactively with the parameter names as listed above and setting.
+
+### Local file custom_config.py
+You can also run the Docker container with a ```-v $(pwd):/config``` parameter (considering the config directory mounts) and if there is a ```custom_config.py``` file, all the values there will override configurable parameters.  If you run into issues with this method, mount the volume and use ```--entry-point=/bin/bash``` to run the container without starting the service.  From there look in the ```/config``` directory and run service manually with ```python TwitterCord.py```.
+
+
+Running
+=======
+```Socieidos``` currently is able to monitor and record pictures from the Twitter API.  The Docker container is named ```emccode/twittercord```.
+The following is a Docker command that will run the service with the specified parameters.  
+
+Configuring with a static configuration file can be done by running ```docker run -ti -p 80:8080 -v $(pwd):/config emccode/twittercord``` where you specify a mount of the local directory to ```/config``` where a ```customer_config.py``` file exists.  Environment variables can still be used to override.
+
+
+Configuring with environment variables takes priority over the items configured in a file.
+```docker run -ti -p 80:8080 -e s3_host='ip_or_fqdn' -e s3_port='10101' -e s3_akia='wuser1@sanity.local' -e s3_secret='secret' -e s3_bucket_name=social -e s3_store_in=selfie -e twitter_keys="['app_key1','app_key2','user_key1','user_key2']" -e twitter_filter="{'track':'selfie'}" -e check_geo_coords='True' -e check_retweeted_status='True' -e check_possibly_sensitive='True' -e geo_box_coords_long_lat='-125,24,-14,83' -e s3_connect="boto.connect_s3(lc_s3_akia,lc_s3_secret,is_secure=False,host=lc_s3_host,port=lc_s3_port,calling_format=OrdinaryCallingFormat())" emccode/twittercord```
+
+
+Presentation
+============
+Included is a default HTML page with a carousel to view images.  This is available by default from ```/```.  The page that is being rendered comes from ```/templates/default.html```.
+
+If you would like to modify the page, then you can do so by placing the iterative portion that declares the image tags wherever you please and in whatever format.  
+
+	{% for object in objects %}
+	<li><img src="{{ object['object_url'] }}"></li>
+	{% endfor %}
+
+If you want to completely replace the HTML content, another option is to create a new Docker image from this one that runs an ```ADD``` for specific local sub-directories of ```static``` and ```templates```.  Lots of options.
+
+
+API
+===
+If you are interested in operating this in more of a micro-service architecture, there is a simple API available to expose the objects that are stored.  You can call ```GET /v1/objects``` to get a response as a ```JSON``` object.  This would allow you to use a carousel that leveraged AJAX or some active form of requesting images without refreshing a page.
+
+## Control Plane - Consumption
+```GET /v1/objects```  
+
+OK 200
+
+	   {
+			response = {
+				requested_objects = ['tracker_name']
+				objects[
+					{
+						object_url = "http://img.png",
+						type = "image",
+						source = "twitter",
+						metadata = {
+							text = "blah"
+							screen_name = "blah"
+							geo = [lat.0,long.0]
+						}
+					}
+				]
+			}
+		}
+
+## Docker Build
+	git clone https://github.com/emccode/socieidos
+	cd socieidos/Docker-twittercord
+	docker build -t emccode/twittercord .
+
+
+## Initial Contributors
+Big thanks goes out to #DevHi5 and **emc {code&#7449;}**s
+- Matt Cowger
+- Jonas Rosland
+
+
+Licensing
+---------
+Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except in compliance with the License. You may obtain a copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+Support
+-------
+Please file bugs and issues at the Github issues page. For more general discussions you can contact the EMC Code team at <a href="https://groups.google.com/forum/#!forum/emccode-users">Google Groups</a>. The code and documentation are released with no warranties or SLAs and are intended to be supported through a community driven process.
+
+ 
